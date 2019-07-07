934f4b672cc6039c696ddb516c6dbc37889e4e1b
Arthur Granado <agranado2k@gmail.com>
2019-07-07 16:47:47 +0100

    Implement Formatter

---
+++ lib/cron_expression_parser/formatter.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module CronExpressionParser
+  # Class that manage the command execution
+  class Formatter
+    def table(input)
+      <<~OUTPUT
+        minute        #{format_line(input, :minute)}
+        hour          #{format_line(input, :hour)}
+        day of month  #{format_line(input, :day_of_month)}
+        month         #{format_line(input, :month)}
+        day of week   #{format_line(input, :day_of_week)}
+        command       #{input[:command]}
+      OUTPUT
+    end
+
+    private
+
+      def format_line(input, type)
+        input[type].join(' ')
+      end
+  end
+end
